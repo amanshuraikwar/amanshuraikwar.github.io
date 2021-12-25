15a3ec686cfe1c1c47c4e86af663174b2d0414fe
amanshuraikwar <amanshuraikwar.in@gmail.com>
2021-12-25 12:41:28 +0000

    Deploying to gh-pages from  @ 43cf13ec8169efe6481feeff5864fab29d8fc658 🚀

---
+++ js/packages/Portfolio-shared-test/adapter-browser.js
@@ -0,0 +1,2 @@
+require("/Users/runner/work/amanshuraikwar.github.io/amanshuraikwar.github.io/project-files/Portfolio/build/js/packages_imported/kotlin-test-js-runner/1.5.31/kotlin-test-karma-runner.js")
+module.exports = require("/Users/runner/work/amanshuraikwar.github.io/amanshuraikwar.github.io/project-files/Portfolio/build/js/packages/Portfolio-shared-test/kotlin/Portfolio-shared-test.js")
